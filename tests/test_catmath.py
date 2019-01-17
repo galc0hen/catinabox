a87603ebffc887d2b65216a0bd177186233841f9
galc0hen <gal.c@sygnia.co>
2019-01-17 16:44:07 +0200

    additional answers

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,19 +1,34 @@
+import pytest
 from catinabox import catmath
 
 
+@pytest.mark.parametrize('age, expected_result', [(7, 35), (0.5, 2.5), (0, 0)])
+def test__cat_years_to_hooman_years__succeeds(age, expected_result):
+    assert catmath.cat_years_to_hooman_years(age) == expected_result
+
+
+"""
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    cat_age = 7
+    hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert hooman_age == 35
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    cat_age = 0.5
+    hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert hooman_age == 2.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
-
+    cat_age = 0
+    hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert hooman_age == 0
 
 # BONUS MATERIAL FOR STEP 2
 
+
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
+
+"""
